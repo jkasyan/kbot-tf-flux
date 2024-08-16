e94fc75be467c13a891648a99a453242bd6a054c
evgenii.kasyan <evgenii.kasyan@maxpay.com>
2024-08-16 18:54:06 +0300

    init

--- main.tf
+++ main.tf
@@ -11,18 +11,18 @@ module "gke_cluster" {
 }
 
 module "github_repository" {
-  source = "github.com/den-vasyliev/tf-github-repository"
-  github_owner = var.GITHUB_OWNER
-  github_token = var.GITHUB_TOKEN
-  repository_name = var.FLUX_GITHUB_REPO
-  public_key_openssh = module.tls_private_key.public_key_openssh
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
   public_key_openssh_title = "flux0"
 }
 
 module "flux_bootstrap" {
-  source = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.GITHUB_REPO}"
-  private_key = module.tls_private_key.private_key_pem
-  config_path = module.gke_cluster.kubeconfig
-  github_token = var.GITHUB_TOKEN
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
+  github_token      = var.GITHUB_TOKEN
 }
